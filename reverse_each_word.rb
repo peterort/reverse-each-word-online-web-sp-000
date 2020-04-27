08827e1597925063bbce605b47bfd326a5cc1fab
Peter Ort <peter@peterort.com>
2020-04-27 17:06:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
-def reverse_each_word(string)
-    string.each do |reversed|
-    end
+#def reverse_each_word(string)
+#    string.each do |reversed|
+#    end
+#end
+
+def reverse_each_word(sentence)
+  sentence.split.collect { |word| word.reverse }.join(" ")
 end
